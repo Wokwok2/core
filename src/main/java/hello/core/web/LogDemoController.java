d32ca7f5ef18f23bc8aa01dc8bfea5d709ec2601
applzz <hws0602@naver.com>
2024-02-19 20:06:14 +0900

    강의 종료

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,8 +2,13 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,6 +17,16 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
 
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
 
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
 
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
 }
